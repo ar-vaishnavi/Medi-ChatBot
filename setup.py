1e6199d49cf1a2b2a21cf4f6805621ec9c9d9954
ar-vaishnavi <arvaishraghu27@gmail.com>
2024-12-30 21:40:52 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'ChatBot using NLP Project',
+    version= '0.0.0',
+    author= 'A R Vaishnavi',
+    author_email= 'arvaishraghu27@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
